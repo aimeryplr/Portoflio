7b3cb8cb9a7d2554475e78d2ebec93455572d779
Aimery <48866596+arzox@users.noreply.github.com>
2024-03-19 15:14:48 +0100

    Add files via upload

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,11 +9,12 @@ import OceanRay from "../components/OceanRay";
 import PlaceHolder from "../assets/placeholder.png";
 import ImgAndText from "../components/ImgAndText.jsx";
 import HorizontalBar from "../components/HorizontalBar.jsx";
+import textrelou from "../assets/textrelou.json";
 
 
 function Projects() {
     const MartialWorldText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo in ligula suscipit ullamcorper necsit amet nisi."
-     + " Nullam sit amet tellus lorem. Fusce maximus nibh sit amet tellus elementum dictum."
+     + " Nullam sit amet tellus lorem. Fusce maximus nibh sit amet tellus elementum dictum.";
 
     const testRef = useRef();
     const projectRef = useRef([]);
@@ -35,7 +36,7 @@ function Projects() {
 
     return (
         <>
-        <section className="flex justify-center align-middle relative" style={{height: "98vh"}}>
+        <section className="flex justify-center align-middle absolute w-full" style={{height: "98vh"}}>
             <OceanRay ref={e => (rayRef.current[0] = e)} rotate={"17"} left={"7%"} scale={"Y(2.25)"} opacity={55}/>
             <OceanRay ref={e => (rayRef.current[1] = e)} rotate={"13"} left={"25%"} scale={"(2.25)"} opacity={75}/>
             <OceanRay ref={e => (rayRef.current[2] = e)} rotate={"8"} left={"37%"} scale={"Y(1.9)"} opacity={45}/>
@@ -43,22 +44,25 @@ function Projects() {
             <OceanRay ref={e => (rayRef.current[4] = e)} rotate={"355"} left={"60%"} scale={"Y(2.3)"} opacity={30}/>
             <OceanRay ref={e => (rayRef.current[5] = e)} rotate={"350"} left={"73%"} scale={"Y(2)"} opacity={45}/>
             <OceanRay ref={e => (rayRef.current[6] = e)} rotate={"330"} left={"95%"} scale={"(2)"} opacity={75}/>
-            {/*3 Projects*/}
-            <div className="flex justify-between items-center space-x-10 mx-[10%] w-full">
-                <ProjectPreview ref={e => (projectRef.current[1] = e)} onClick={() => scrollToPos(posMartial)} image={RobloxImg}
-                                text="Martial World" bgColor="bg-cyan-400 cursor-pointer"/>
-                <ProjectPreview ref={e => (projectRef.current[0] = e)} onClick={() => scrollToPos(posPygame)} image={PygameImg}
-                                text="Planet Explorer" bgColor="bg-[#2C3233] cursor-pointer"/>
-                <ProjectPreview ref={e => (projectRef.current[2] = e)} onClick={() => scrollToPos(posElydia)} image={MinecraftServer}
-                                text="Elydia" bgColor="bg-cyan-400 cursor-pointer"/>
-            </div>
         </section>
+        <div className="flex flex-wrap min-h-[88vh] gap-6 justify-start items-center mx-[8%] my-6">
+                <ProjectPreview ref={e => (projectRef.current[1] = e)} onClick={() => scrollToPos(posMartial)} image={RobloxImg}
+                                text="Martial World" bgColor="bg-blue-400 cursor-pointer"/>
+                <ProjectPreview ref={e => (projectRef.current[0] = e)} onClick={() => scrollToPos(posPygame)} image={PygameImg}
+                                text="Planet Explorer" bgColor="bg-blue-400 cursor-pointer"/>
+                <ProjectPreview ref={e => (projectRef.current[2] = e)} onClick={() => scrollToPos(posElydia)} image={MinecraftServer}
+                                text="Elydia" bgColor="bg-blue-400 cursor-pointer"/>
+                <ProjectPreview ref={e => (projectRef.current[3] = e)} onClick={() => scrollToPos(posElydia)} image={MinecraftServer}
+                                text="Elydia" bgColor="bg-blue-400 cursor-pointer"/>
+                <ProjectPreview ref={e => (projectRef.current[4] = e)} onClick={() => scrollToPos(posElydia)} image={MinecraftServer}
+                                text="Elydia" bgColor="bg-blue-400 cursor-pointer"/>
+        </div>
         <HorizontalBar color="bg-cyan-400"/>
-        <ImgAndText name="Martial World" text={MartialWorldText} setPos={setPosMartial} image={PlaceHolder} style="bg-slate-100 border-x-1"/>
+        <ImgAndText name="Martial World" text={MartialWorldText} setPos={setPosMartial} image={PlaceHolder} style="bg-slate-100"/>
         <HorizontalBar color="bg-sky-400"/>
-        <ImgAndText name="Planet Explorer" text={MartialWorldText} isImageLeft={true} setPos={setPosPygame} image={PlaceHolder} style="bg-slate-100"/>
+        <ImgAndText name="Planet Explorer" text={textrelou.projects.martialWorld.desc} isImageLeft={true} setPos={setPosPygame} image={PlaceHolder} style="bg-slate-100"/>
         <HorizontalBar color="bg-blue-400"/>
-        <ImgAndText name="Elydia" text={MartialWorldText} setPos={setPosElydia} image={PlaceHolder} style="bg-slate-100 border-x-1"/>
+        <ImgAndText name="Elydia" text={MartialWorldText} setPos={setPosElydia} image={PlaceHolder} style="bg-slate-100"/>
         </>
     )
 }
