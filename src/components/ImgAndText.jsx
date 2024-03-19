ce087a95da157340cd4e29b1390a8eab76a4c74b
Aimery <48866596+arzox@users.noreply.github.com>
2024-03-19 15:15:32 +0100

    Add files via upload

--- src/components/ImgAndText.jsx
+++ src/components/ImgAndText.jsx
@@ -17,20 +17,20 @@ function ImgAndText(props) {
             end: "top top",
             toggleActions: "play reverse restart reverse"
         },
-        x: props.isImageLeft ? "-80%" : "80%",  opacity: 0, duration: 0.5, ease: "circ"});
+        x: props.isImageLeft ? "-40%" : "40%",  opacity: 0, duration: 0.5, ease: "circ"});
         props.setPos(sectionRef.current.getBoundingClientRect().top - (window.innerHeight - sectionRef.current.getBoundingClientRect().height + 80) / 2)
     }, []); 
 
     return (
         <section ref={sectionRef} className={"relative" + ` ${props.style}`}>
             <h1 className="text-center text-slate-[#2C3233] font-bold text-5xl absolute left-1/2 top-10 transform -translate-x-1/2">{props.name}</h1>
-            <span className="px-[2%] h-[70vh] flex justify-around gap-7 items-center overflow-x-hidden">
+            <span className="px-[4%] h-[70vh] flex justify-around gap-7 items-center overflow-x-hidden">
                 {props.isImageLeft ? 
-                <><img src={props.image} ref={imgRef} className="rounded-lg border-4 border-slate-50 drop-shadow-xl w-5/6" alt="placeholder"/>
-                <h2 ref={textRef} className="text-slate-[#2C3233] text-4xl font-semibold text-justify w-full">{props.text}</h2></>
+                <><img src={props.image} ref={imgRef} className="rounded-lg border-4 border-slate-50 drop-shadow-xl w-1/3" alt="placeholder"/>
+                <h2 ref={textRef} className="text-slate-[#2C3233] text-4xl font-semibold text-justify w-2/3">{props.text}</h2></>
                 :
-                <><h2 ref={textRef} className="text-slate-[#2C3233] text-4xl font-semibold text-justify w-full">{props.text}</h2>
-                <img src={props.image} ref={imgRef} className="rounded-lg border-4 border-slate-50 w-5/6 drop-shadow-xl" alt="placeholder"/></>}
+                <><h2 ref={textRef} className="text-slate-[#2C3233] text-4xl font-semibold text-justify w-2/3">{props.text}</h2>
+                <img src={props.image} ref={imgRef} className="rounded-lg border-4 border-slate-50 w-1/3 drop-shadow-xl" alt="placeholder"/></>}
             </span>
         </section>
     )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,16 +9,19 @@ function Link({name, route}) {
 
 function NavBar() {
   return (
-    <header className="flex fixed justify-between w-screen items-center bg-cyan-400 h-20 z-50 px-28">
-        <NavLink to="/" className='z-20'>
-           <img className='drop-shadow-xl h-14' src={mask}/>
-        </NavLink>
-        <div className='flex space-x-12 items-center'>
-            <Link name="Projects" route="/Projects"/>
-            <Link name="About" route="/About"/>
-            <Link name="Contact" route="/Contacts"/>
-        </div>
-    </header>
+    <>
+      <header className="flex fixed justify-between w-screen items-center bg-cyan-400 h-20 z-50 px-28">
+          <NavLink to="/" className='z-20'>
+            <img className='drop-shadow-xl h-14' src={mask}/>
+          </NavLink>
+          <div className='flex space-x-12 items-center'>
+              <Link name="Projects" route="/Projects"/>
+              <Link name="About" route="/About"/>
+              <Link name="Contact" route="/Contacts"/>
+          </div>
+      </header>
+      <div className='relative h-20 bg-transparent'></div>
+    </>
   )
 }
 
